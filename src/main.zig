e0819d5da97f1ec26bf8fa9a81d705f4749c9d5e
Pratik Giri <pratikgiri043@gmail.com>
2025-10-04 16:29:58 +0530

    first zig commit

---
+++ src/main.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+const Slave = struct {
+    age: usize,
+    name: [6]u8,
+    salary: f32,
+};
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    var ptr = try allocator.create(Slave);
+    defer allocator.destroy(ptr);
+
+    ptr.age = 35;
+    _ = try std.fmt.bufPrint(&ptr.name, "Steve", .{});
+    ptr.salary = 100.00;
+
+    std.debug.print(
+        "Age: {}\nName: {s}\nSalary: {d:.2}\n",
+        .{ ptr.age, ptr.name, ptr.salary },
+    );
+
+    ptr.age = 40;
+    _ = try std.fmt.bufPrint(&ptr.name, "Bill", .{});
+    ptr.salary = 1000.00;
+
+    std.debug.print(
+        "Age: {}\nName: {s}\nSalary: {d:.2}\n",
+        .{ ptr.age, ptr.name, ptr.salary },
+    );
+}
